b1a6031a7225131721bb72da2463d7a817a3087c
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-18 14:18:59 -0500

    ahh woops vim thought we were in denoland

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = (): JSX.Element => {
   return (
@@ -21,6 +21,6 @@ const App = (): JSX.Element => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
