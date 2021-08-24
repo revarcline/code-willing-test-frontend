2a9429a525320159f718b2c559d2e62665ec489d
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-24 10:24:27 -0500

    slice type?

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,3 +9,9 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
